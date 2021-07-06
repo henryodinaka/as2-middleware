5cd4ade3192c60e61a81ec308272b34467574a81
Henry Odinaka <deleo.henry@gmail.com>
2021-07-06 11:08:18 +0100

    Updated the code to enable uploads to generic folder

--- src/main/java/connect/as2/as2middleware/service/impl/EntryServiceImpl.java
+++ src/main/java/connect/as2/as2middleware/service/impl/EntryServiceImpl.java
@@ -4,6 +4,7 @@ import connect.as2.as2middleware.config.APIException;
 import connect.as2.as2middleware.service.EntryService;
 import connect.as2.as2middleware.utils.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,6 +20,11 @@ public class EntryServiceImpl implements EntryService {
 
     private final FileService fileService;
 
+    @Value("${as2.partners.my-company.id}")
+    private String companyId;
+    @Value("#{'${partners.id}'.split(',')}")
+    private List<String> partnerIds;
+
     public EntryServiceImpl(FileService fileService) {
         this.fileService = fileService;
     }
@@ -29,19 +35,29 @@ public class EntryServiceImpl implements EntryService {
             throw new APIException(new Response("16", "File to be uploaded cannot be null", null));
         var originalFilename = file.getOriginalFilename();
         var split = originalFilename.split("-");
-        if (split.length != 3)
-            throw new APIException(new Response("16", "Invalid file name. It must be in this format: MyCompany-YourCompany-TheEdiFileNameToBeSent.<file extension>", null));
-        fileService.save("Any",file);
+        if (split.length < 3)
+            throw new APIException(new Response("16", "Invalid file name. It must be in this format: MyCompany_ID-Partner_ID-TheEdiFileNameToBeSent.<file extension>", null));
+        var companyID = split[0];
+        var partnerID = split[1];
+        if (!companyId.equals(companyID))
+            throw new APIException(new Response("16", "Invalid companyID. It must be: " + companyId, null));
+
+        if (!partnerIds.contains(partnerID))
+            throw new APIException(new Response("16", "This partner has not been configured to receive messages, See the configured partner Ids: " + partnerIds, null));
+
+        fileService.save("Any", file);
         return new Response(200, "00", "File Dropped successfully", null);
     }
-    public Response sendToOnePartner(String partnerName,MultipartFile file) {
+
+    public Response sendToOnePartner(String partnerName, MultipartFile file) {
 
         if (file == null)
             throw new APIException(new Response("16", "File to be uploaded cannot be null", null));
-        fileService.save(partnerName,file);
+        fileService.save(partnerName, file);
         return new Response(200, "00", "File Dropped successfully", null);
     }
-    public Response sendReverse(String partnerName,MultipartFile file) {
+
+    public Response sendReverse(String partnerName, MultipartFile file) {
 
         if (file == null)
             throw new APIException(new Response("16", "File to be uploaded cannot be null", null));
@@ -52,6 +68,7 @@ public class EntryServiceImpl implements EntryService {
     public List<File> loadAllMDNOut() {
         return fileService.loadAllMDN(false);
     }
+
     public List<File> loadAllMDNIn() {
         return fileService.loadAllMDN(true);
     }
